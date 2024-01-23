5cef030e28383369595d7f015a901f9bcc58b784
hskimse <hskimse1@gmail.com>
2024-01-23 02:41:04 +0000

    add parse argv lists

--- 4_CacheLab/cachelab-handout/csim.c
+++ 4_CacheLab/cachelab-handout/csim.c
@@ -1,7 +1,60 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
-int main()
+struct {
+    int verbose;
+    int s;
+    int E;
+    int b;
+} option;
+
+FILE *tracefile;
+
+int parse_args(int argc, char *argv[]);
+void useage(char *name);
+
+int main(int argc, char *argv[])
 {
     printSummary(0, 0, 0);
     return 0;
+}
+
+int parse_args(int argc, char *argv[])
+{
+    int ch;
+    while ((ch = getopt(argc, argv, "hvs:E:b:t:")) != -1)
+        switch(ch) {
+        case 'h':
+            useage(argv[0]);
+            break;
+        case 'v':
+            option.verbose = 1;
+            break;
+        case 's':
+            option.s = atoi(optarg);
+            break;
+        case 'E':
+            option.E = atoi(optarg);
+            break;
+        case 'b':
+            option.b = atoi(optarg);
+            break;
+        case 't':
+            tracefile = fopen(optarg, "r");
+            break;
+        case '?':
+        default:
+            useage(argv[0]);
+            break;
+        }
+    return ch;
+}
+
+void useage(char *name)
+{
+    fprintf(stderr, "Useage: ./%s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n", name);
+    exit(1);
 }
