1dda6112bc45c4b80bb40a17fb2ee1d81f0664b5
hskimse <hskimse1@gmail.com>
2024-03-21 07:15:54 +0000

    Implement function mm_malloc using implicit free list.
    Helper function -- target_block - traverse the free list and return
    the pointer to header of appropriate free block, is_allocated -
    Check if LSB of header is 1, set_allocated - set LSB of p 1;
    i.e. mark header to allocated -- also implemented.
    
    Detailed description of allignment method will be presented by next
    commit. (At header of this file.)

--- 6_MallocLab/malloclab-handout/mm.c
+++ 6_MallocLab/malloclab-handout/mm.c
@@ -1,13 +1,9 @@
 /*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
+ * mm.c - memory allocation module using implicit free list.
  *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ * Slow, but good memory efficiancy.
+ * ----
+ * NOTE: This code is for 32-bit system; Compile it with -m32 flag.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,6 +13,11 @@
 
 #include "mm.h"
 #include "memlib.h"
+
+/* My helper routines */
+static void *target_block(int blocksize);
+static int is_allocated(size_t header);
+static int set_allocated(size_t *p);
 
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
@@ -29,6 +30,8 @@ team_t team = {
     "Dillution",
     /* First member's email address */
     "hskimse1@gmail.com",
+    "",
+    ""
 };
 
 /* single word (4) or double word (8) alignment */
@@ -37,30 +40,56 @@ team_t team = {
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
+/* sizeof(size_t) = 4, ALIGN(4) = 8 */
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* 8 - 4 */
+#define SIZE_T_PADDING SIZE_T_SIZE - sizeof(size_t)
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    void *p;
+    if ((p = mem_sbrk(SIZE_T_SIZE)) == (void *)-1)
+        return -1;
+    p = (void *)((char *)p + SIZE_T_PADDING); /* Set p as pointer to header */
+    *(size_t *)p = 0;
+    set_allocated(p);
+    
     return 0;
 }
 
 /* 
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
+ * mm_malloc - Allocate appropriate free block by tranversing
+ *        implicit free list.
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
+    const int blksize = ALIGN(size + SIZE_T_SIZE); /* Block size to malloc */
+    void *p;                                   /* Pointer to malloced block */
+    
+    /* Traverse the free list. */
+    if ((p = target_block(blksize)) != NULL)
+        return (void *)((char *)p + sizeof(size_t));
+    
+    /* There is no appropriate free block on the heap. */
+    p = mem_sbrk(blksize);
+    if (p == (void *)-1) {
 	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    } else {
+        /* Store block size to header */
+        size_t *hp = (size_t *)((char *)p - sizeof(size_t));
+        *hp = blksize;
+        set_allocated(hp);
+
+        /* Set header of last block as 0/1 */
+        size_t *lhp = (size_t *)((char *)hp + blksize);
+        *lhp = 0;
+        set_allocated(lhp);
+        
+        return (void *) p;      /* return pointer to payload */
     }
 }
 
@@ -91,16 +120,52 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 }
 
+/**********************
+ * My helper routines *
+ **********************/
 
+/*
+ * target_block - return the pointer to head of appropriate free block while
+ *       traverse the implicit list. return NULL if there is no space.
+ */
+static void *target_block(int blocksize)
+{
+    char *p = (char *) mem_heap_lo() + SIZE_T_PADDING; /* Pointer to each
+                                                        * block's header */
+    size_t size = *(size_t *)p & ~0x7;   /* Blocksize of current iteration. */
+    void *dest = NULL;                   /* Target block. */
+    size_t min = -1U;              /* Minimum blocksize for entire iteration */
 
+    while (size != 0) {
+        /* If p is the appropriate block, load it to dest */
+        if (!is_allocated(*p) && size >= blocksize && size < min) {
+            dest = p;
+            min = size;
+        }
+        
+        /* Traverse to the next block */
+        p += size;
+        size = *(size_t *)p & ~0x7;
+    }
+    return dest;
+}
 
+/*
+ * is_allocated - Check if LSB of header is 1
+ */
+static int is_allocated(size_t header)
+{
+    return header & 0x1;
+}
 
+static int set_allocated(size_t *p)
+{
+    if (p == NULL)
+        return -1;
+    *p |= 0x1;                  /* set LSB to 1 */
+    return 0;
+}
 
-
-
-
-
-
-
-
-
+/*****************************
+ * End of My helper routines *
+ *****************************/
