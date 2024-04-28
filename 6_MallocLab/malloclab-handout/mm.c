aab86c0ae1f29ff7f569c2878fcf2ac3faf645d8
dilluti0n <hskimse1@gmail.com>
2024-04-28 15:50:12 +0900

    Implement allocater that uses explicit free list
    - Add flag FTR_VALID for footer to indicate whether it is overwritten
    as list node. (This happens only if blocksize is 8.)
    - Implement node editing functions. (node_*)
    - Add few lines to mm_malloc to handle explicit list appropriatly.
    - On mm_free, when merging with consecutive blocks, all four cases are
    handdled diffrently. (prev alloc/free, next alloc/free) And this cases
    are indicated by internal variable isnotmerged.
    - One more global variable for get_target_block - prev_node.
    - Handling the tided struct of list is defered to get_target_block
    from mm_free, case (prev alloc, next free).

--- 6_MallocLab/malloclab-handout/mm.c
+++ 6_MallocLab/malloclab-handout/mm.c
@@ -71,12 +71,19 @@
 #include "memlib.h"
 
 static int block_size;          /* Size of block founded by get_target_block */
+static void **prev_node;        /* Pointer to previous free block's node */
 static void **TOP;              /* Top node of explicit free list */
-static void **BOT;              /* Bottom node for explicit free list */
 
 static void *get_target_block(size_t blocksize);
 inline static int set_header(void *header, size_t blocksize, int flag);
 inline static int set_footer(void *header, size_t blocksize);
+inline static void **node_search_prev(void **curr);
+inline static void node_delete(void **prev, void **curr);
+inline static void node_delete_s(void **curr);
+inline static void node_replace(void **prev, void **src, void **dst);
+inline static void node_replace_s(void **src, void **dst);
+inline static void node_top(void **node);
+inline static void node_insert(void **prev, void **node);
 
 /*********************************************************
  * NOTE TO STUDENTS: Before you do anything else, please
@@ -115,6 +122,7 @@ team_t team = {
 
 #define IS_ALLOC(header) ((header)&HDR_ALLOC)
 #define IS_PFREE(header) ((header)&HDR_PFREE)
+#define IS_VALID_FTR(footer) ((footer)&FTR_VALID)
 
 /* Macros for parse size and flag for header element. */
 #define MM_SIZE(header) ((header)&~HDR_MASK)
@@ -123,9 +131,11 @@ team_t team = {
 /* flags for function set_header */
 #define HDR_FREE 0
 #define HDR_ALLOC 1         /* This block is allocated */
-#define HDR_PFREE 2           /* Previous block is free */
+#define HDR_PFREE 2         /* Previous block is free */
 
-#define DEBUG
+#define FTR_VALID 4
+
+//#define DEBUG
 
 #ifdef DEBUG
 
@@ -138,9 +148,10 @@ static void dbg_print_list();
 
 #define DBG_CHECK \
     printf("[%3i]: %s returns\n",__LINE__, __func__);\
-    dbg_print_heap(1);                           \
+    dbg_print_heap(0);\
     dbg_print_list();\
     dbg_heap_check();\
+    dbg_list_check();
 
 #else
 #define DBG_CHECK
@@ -180,18 +191,21 @@ void *mm_malloc(size_t size)
     if ((p = get_target_block(blksize)) != NULL) {
         const size_t tmp = block_size - blksize;       /* Size of empty space */
 
-        /* Mark target block for use */
+        /* Mark target block as use */
         set_header(p, blksize, HDR_ALLOC);
 
         /* Mark empty space as free if there is empty space */
         void *next = p + blksize;
         if (tmp != 0) {
-            set_header(next, (size_t) tmp, HDR_FREE);
-            set_footer(next, (size_t) tmp);
+            set_header(next, (size_t)tmp, HDR_FREE);
+            set_footer(next, (size_t)tmp);
+            node_replace(prev_node, (void **)p + 1, (void **)next + 1);
         } else {                /* There is no empty space */
             /* Mark next block as "Previous alloc" */
             *(size_t *)next = *(size_t *)next & ~HDR_PFREE;
+            node_delete(prev_node, (void **)p + 1);
         }
+
         DBG_CHECK
         return (void *)((size_t *)p + 1);        /* Return pointer to payload */
     }
@@ -206,6 +220,7 @@ void *mm_malloc(size_t size)
         const size_t psize = MM_SIZE(*((size_t *)p - 1));
         excess -= psize;
         p -= psize;                       /* New block should start at here. */
+        node_delete_s((void **)(p + sizeof(size_t)));
     }
 
     /* Request more heap !! */
@@ -232,38 +247,50 @@ void mm_free(void *ptr)
     int isnotmerged = 1;
 
     /* Merge with previous block */
-    size_t tmp;
     if (IS_PFREE(hdr)) {
-        tmp = *((size_t *)p - 1);        /* Header element of previous block */
+        size_t tmp;
+        tmp = *((size_t *)p - 1); /* Header element of previous block */
+        if (!IS_VALID_FTR(tmp))
+            /* issue; Only 8-byte block's footer is overrided by list node. */
+            tmp = *((size_t *)p - 2);
 
         /* Set flag and blocksize and header */
         const size_t pbsize = MM_SIZE(tmp); /* Size of previous block */
         blksize += pbsize;
         p -= pbsize;            /* Merge two blocks */
-
-        /* We can consider prev block's node as our node */
-        curr = (void **)((void *)curr - pbsize);
-
         isnotmerged = 0;
     }
 
     /* Merge with next block */
-    tmp = *(size_t *)(p + blksize);   /* Header element of next block */
-    if (!IS_ALLOC(tmp)) {
-        blksize += MM_SIZE(tmp);
-                                         /* Pointer to next block's list node */
-        void **next = (void *)p + blksize + sizeof(size_t);
-        *curr = *next;
-        /* If current block has not been merged with prev block, construct
-         * the linked list like (p) -> next -> curr -> (n) from the origi-
-         * -nal one (p) -> next -> (n), mark the unused next block's header
-         * as -1, and defer the handling to the function get_target_block
-         * since we cannot access to (p) here.
-         */
+    void *nh = p + blksize;                  /* Pointer to next block's header */
+    size_t nhele = *(size_t *)nh;            /* Header element of next block */
+    if (!IS_ALLOC(nhele)) {
+        /* Pointer to next block's list node */
+        void **next = nh + sizeof(size_t);
         if (isnotmerged) {
-            *next = (void *)curr;
-            *(size_t *)(p + blksize) = -1U;
+            /**
+             * prev alloc, next free
+             * Construct the list like (p) -> next -> curr -> (n) from the origi-
+             * -nal one (p) -> next -> (n), mark the unused next block's header
+             * as -1, and defer the handling to the function get_target_block
+             * since we cannot access to (p) here.
+             */
+            if (MM_SIZE(nhele) > 8) {
+                node_insert(next, curr);
+                *(size_t *)nh = -1U;
+            } else {
+                /* TODO: improve this to not search entire list */
+                /* This case, footer of curr block will overlap node "next" */
+                node_replace_s(next, curr);
+            }
+        } else {
+            /**
+             * prev free, next free
+             * Just deleting next block on free list would be fine.
+             */
+            node_delete_s(next);
         }
+        blksize += MM_SIZE(nhele);
 
         isnotmerged = 0;
     }
@@ -276,14 +303,14 @@ void mm_free(void *ptr)
     void *np = p + blksize;                /* Pointer to next block's header */
     set_header(np, MM_SIZE(*(size_t *)np), HDR_PFREE | HDR_ALLOC);
 
-    /* If curr block is not merged, append it to BOT of explicit free list */
-    if (isnotmerged) {
-        *curr = NULL;
-        *BOT = curr;            /* Note: BOT is initialized as &TOP */
-        BOT = curr;             /* Mark curr as bottom */
-    }
+    /* If curr block is not merged, append it to TOP of free list */
+    if (isnotmerged) /* prev alloc, next alloc */
+        node_top(curr);
+
+    /* prev free, next alloc :Do nothing. */
+
     DBG_CHECK
-}
+    }
 
 /*
  * mm_realloc - Change the size of the allocation pointed to by ptr to size, and
@@ -329,30 +356,47 @@ void *mm_realloc(void *ptr, size_t size)
 
 /*
  * get_target_block - return the pointer to head of appropriate free block while
- *       traversing the implicit list. return NULL if there is no space.
+ *       traversing the explicit free list. return NULL if there is no space.
  *
  *       Set global variable block_size as the founded block's size to specify
  *       the size of empty block after allocate.
  */
 static void *get_target_block(size_t blocksize)
 {
-                                            /* Pointer to each block's header */
-    char *p = (char *)mem_heap_lo() + SIZE_T_PADDING;
-    size_t size = MM_SIZE(*(size_t *)p);   /* Blocksize of current iteration. */
-    void *dest = NULL;                        /* Target block. */
-    size_t min = -1U;              /* Minimum blocksize for entire iteration */
+    void **p = TOP;                 /* Pointer to each free block's node */
+    size_t size;                    /* Blocksize of each iteration. */
+    void *dest = NULL;              /* Pointer to header of target block */
+    size_t min = -1U;               /* Minimum blocksize for entire iteration */
+    void **prev = (void **)&TOP;    /* Previous node for current ineration */
 
-    while (size != 0) {
-        /* If p is the appropriate block, load it to dest */
-        if (!IS_ALLOC(*p) && size >= blocksize && size < min) {
-            dest = p;
+    while (p != NULL) {
+        void *hp = (void *)((size_t *)p - 1);     /* Pointer to header */
+        size_t header = *(size_t *)hp;  /* Header element of each block */
+
+        /**
+         * Resolve prev -> p -> *p -> (n) to prev -> *p -> (n) and move
+         * current iteration to *p. (which is real adress to block node)
+         */
+        while (header == -1U) {
+            node_delete(prev, p);
+            if ((p = *p) == NULL)
+                return dest;
+            hp = (void *)(p - 1);
+            header = *(size_t *)hp;
+        }
+        size = MM_SIZE(header);
+
+        /* If the block is appropriate, load hp to dest */
+        if (size >= blocksize && size < min) {
+            prev_node = prev;
+            dest = hp;
             min = size;
             block_size = size;    /* Load this to indicate found block's size */
         }
 
-        /* Traverse to the next block */
-        p += size;
-        size = MM_SIZE(*(size_t *)p);
+        /* Traverse to the next list node */
+        prev = p;
+        p = *p;
     }
     return dest;
 }
@@ -388,10 +432,73 @@ inline static int set_footer(void *header, size_t blocksize)
 
     /* Duplicate header to footer */
     fp = (size_t *)(header + blocksize - sizeof(size_t));
-    *fp = *(size_t *)header;
+    *fp = (*(size_t *)header) | FTR_VALID;
     return 0;
 }
 
+/*
+ * node_top - Put node to the TOP of list.
+ */
+inline static void node_top(void **node)
+{
+    *node = TOP;
+    TOP = node;
+}
+
+/*
+ * node_insert - insert node next to the prev node. prev should not be NULL.
+ */
+inline static void node_insert(void **prev, void **node)
+{
+    *node = *prev;
+    *prev = node;
+}
+
+inline static void **node_search_prev(void **curr)
+{
+    void **p = TOP, **prev = (void **)&TOP;
+    while (p != curr) {
+        if (p == NULL) {
+            fprintf(stderr,
+               "fatal error: attempt to search node %p but it is not on list\n"\
+               "header: %x\n", curr, *(size_t *)(curr - 1));
+            exit(1);
+        }
+        prev = p;
+        p = *p;
+    }
+    return prev;
+}
+/*
+ * node_delete - Delete current node from explicit free list. prev should
+ *  be always non-null, initialized as &TOP. (i.e. if TOP = curr, call of
+ *  this function makes TOP = *curr.)
+ */
+inline static void node_delete(void **prev, void **curr)
+{
+    *prev = *curr;
+}
+
+inline static void node_delete_s(void **curr)
+{
+    void **prev = node_search_prev(curr);
+    node_delete(prev, curr);
+}
+
+/*
+ * node_replace - replace list node src by dst.
+ */
+inline static void node_replace(void **prev, void **src, void **dst)
+{
+    *prev = dst;
+    *dst = *src;
+}
+
+inline static void node_replace_s(void **src, void **dst)
+{
+    void **prev = node_search_prev(src);
+    node_replace(prev, src, dst);
+}
 /*****************************
  * End of My helper routines *
  *****************************/
